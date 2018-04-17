0aabdf34f32507066c02f58c307479b6d9a3a0c7
U-JASON\jacko <jackonly06@hotmail.com>
2018-04-17 23:38:58 +0800

    add new file

---
+++ ch07/7_31.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+    > File Name: 7_31.h
+    > Author: N.Jie
+    > Mail: jackonly06@hotmail.com
+    > Created Time: Tue, 17 Apr, 2018 11:35:31 PM
+ ************************************************************************/
+
+#ifndef _DJX_7_31_H_
+#define _DJX_7_31_H_
+
+class Y;
+
+class X{
+    Y *y = nullptr;
+};
+
+class Y{
+    X x;
+};
+
+#endif //_DJX_7_31_H_
