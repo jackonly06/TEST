b360f58f43771b65243982813b24ff66456d549a
U-JASON\jacko <jackonly06@hotmail.com>
2018-05-26 23:31:39 +0800

    add new file

---
+++ ch09/9_50.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+    > File Name: 9_50.cpp
+    > Author: N.Jie
+    > Mail: jackonly06@hotmail.com
+    > Created Time: Sat, 26 May, 2018 11:24:33 PM
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int sum_for_int(const vector<string> &v)
+{
+    int sum = 0;
+    for( auto const &s : v ){
+        sum += stoi(s);
+    }
+
+    return sum;
+}
+
+float sum_for_float(const vector<string> &v)
+{
+    float sum = 0.0;
+    for( auto const &s : v ){
+        sum += stof(s);
+    }
+
+    return sum;
+}
+
+int main(int argc, char** argv)
+{
+    vector<string> v = {"1", "2", "3", "4.5"};
+    cout << sum_for_int(v) << endl;
+    cout << sum_for_float(v) << endl;
+
+    return 0;
+}
+
