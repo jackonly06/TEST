ffbc9f5d21bf40033c1ad6938db71af30552c3bc
U-JASON\jacko <jackonly06@hotmail.com>
2018-06-27 00:10:43 +0800

    add new file

--- ch11/11_14.cpp
+++ ch11/11_14.cpp
@@ -17,31 +17,42 @@ public:
     using Child = pair<string, string>;
     using Children = vector<Child>;
     using Data = map<string, Children>;
-}
 
-void add(string const &last_name, string const &first_name, string birthday)
-{
-    _data[last_name].push_back(make_pair(first_name, birthday));
-}
 
-ostream &print(ostream &os) const
-{
-    if(_data.empty()){
-        return os << "No data right now." << endl;
+    void add(string const &last_name, string const &first_name, string birthday)
+    {
+        _data[last_name].push_back(make_pair(first_name, birthday));
     }
 
-    for(const auto &pair : _data){
-        os << pair.first << ":\n";
-        for(const auto &child : pair.second){
-            os << child.first << " " << child.second << endl;
+    ostream &print(ostream &os) const
+    {
+        if(_data.empty()){
+            return os << "No data right now." << endl;
         }
-        os << endl;
+
+
+        for(const auto &pair : _data){
+            os << pair.first << ":\n";
+            for(const auto &child : pair.second){
+                os << child.first << " " << child.second << endl;
+            }
+            os << endl;
+        }
+        return os;
     }
-    return os;
-}
+private:
+    Data _data;
+};
 
 int main(int argc, char** argv)
 {
+    Families families;
+    string message = "Please enter last name, first name and birthday";
+
+    for(string l, f, b; cout << message << endl, cin >> l >> f >> b; families.add(l, f, b)){
+        ;
+    }
+    families.print(cout << "Current data:" << endl);
 
     return 0;
 }
