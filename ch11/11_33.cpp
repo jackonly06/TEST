a76bd199295011689b44732b6325d9c22a0d1097
U-JASON\jacko <jackonly06@hotmail.com>
2018-07-04 15:43:37 +0800

    add new file

---
+++ ch11/11_33.cpp
@@ -0,0 +1,58 @@
+/*************************************************************************
+    > File Name: 11_33.cpp
+    > Author: N.Jie
+    > Mail: jackonly06@hotmail.com
+    > Created Time: Wed,  4 Jul, 2018 03:18:27 PM
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+map<string, string> buildMap(ifstream &map_file)
+{
+    map<string, string> trans_map;
+    for(string key, value; map_file >> key && getline(map_file, value);){
+        if( value.size() > 1 ){
+            trans_map[key] = value.substr(1).substr(0, 
+                    value.find_last_not_of(' '));
+        }
+    }
+    return trans_map;
+}
+
+const string &transform(const string &s, const map<string, string> &m)
+{
+    auto map_it = m.find(s);
+
+    return map_it == m.cend() ? s : map_it->second;
+}
+
+void word_transform(ifstream &map, ifstream &input)
+{
+    auto trans_map = buildMap(map);
+    for(string text; getline(input, text);){
+        istringstream iss(text);
+        for(string word; iss >> word;){
+            cout << transform(word, trans_map) << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ifstream ifs_map("./test.txt"),
+             ifs_content("./test_transform.txt");
+    if( ifs_map && ifs_content ){
+        word_transform(ifs_map, ifs_content);
+    }else{
+        cerr << "can't find the documents." << endl;
+    }
+    return 0;
+}
+
