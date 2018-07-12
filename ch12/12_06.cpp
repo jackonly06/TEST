9922f93c7eb214de402a7d5da5fb5df18dcd79f3
U-JASON\jacko <jackonly06@hotmail.com>
2018-07-12 23:58:38 +0800

    add new file

---
+++ ch12/12_06.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+    > File Name: 12_06.cpp
+    > Author: N.Jie
+    > Mail: jackonly06@hotmail.com
+    > Created Time: Thu, 12 Jul, 2018 11:55:04 PM
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+vector<int> *dynamic_vector_generator();
+void dynamic_vector_processor(vector<int> *ptr_v);
+void dynamic_vector_printer(vector<int> *ptr_v);
+
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
+
