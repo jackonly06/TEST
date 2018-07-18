7d74717c367cdbe84190e9584fdb8bee08f1e081
U-JACKONLY06\jacko <jackonly06@hotmail.com>
2018-07-18 15:45:42 +0800

    add new file

---
+++ one_point/ch01/1.2EX.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+    > File Name: 1.2EX.cpp
+    > Author: N.Jie
+    > Mail: jackonly06@hotmail.com
+    > Created Time: Wed, Jul 18, 2018  3:42:36 PM
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "Enter two numbers:" << endl;
+    int v1 = 0, v2 = 0;
+    cin >> v1 >> v2;
+    cout << "The sum of " << v1 << " and "<< v2
+         << " is " << v2 + v1 << endl;
+
+    return 0;
+}
+
