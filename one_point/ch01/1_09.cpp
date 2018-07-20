47230d8f5ff90e8f118a778573a13452cdfde4ec
U-JACKONLY06\jacko <jackonly06@hotmail.com>
2018-07-20 23:52:40 +0800

    add new file

---
+++ one_point/ch01/1_09.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+    > File Name: 1_09.cpp
+    > Author: N.Jie
+    > Mail: jackonly06@hotmail.com
+    > Created Time: Fri, Jul 20, 2018 11:50:06 PM
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int sum = 0, val = 50;
+
+    while( val <= 100 ){
+        sum += val;
+        ++val;
+    }
+
+    cout << "the sum is: " << sum << endl;
+
+    return 0;
+}
+
